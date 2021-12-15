37e52e8369856e14f5b3d1b2ab77446b52ac2d83
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-15 12:45:24 +0100

    Dodaj pliki projektów.

---
+++ WWSSE2021PP06_6679/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+    Napisz program, który dla zadanej liczby całkowitej dodatniej oblicza jej reprezentację binarną.
+*/
+
+Console.Write("Podaj liczbę całkowitą dotatnią: ");
+int number = int.Parse(Console.ReadLine());
+
+int remainder;
+string result = "";
+while (number > 0)
+{
+    remainder = number % 2;
+    number /= 2;
+    result = remainder.ToString() + result;
+}
+Console.WriteLine("Reprezentacja binarna podanej liczby wynosi: {0}", result);
+
